2bd264617425fbd2c03f05b785ac8e502edfa696
Wade Wu <wuqingjun@gmail.com>
2017-04-15 08:14:05 -0700

    working!

--- systemdesign/systemdesign/Problems/ConsistentHashing/consistenthashing.h
+++ systemdesign/systemdesign/Problems/ConsistentHashing/consistenthashing.h
@@ -21,7 +21,7 @@ public:
 				if (res[j][1] - res[j][0] > res[index][1] - res[index][0]) index = j;
 			}
 
-			res.push_back({ (res[index][0] + res[index][1]) / 2 + 1, res[index][1], i});
+			res.push_back({ (res[index][0] + res[index][1]) / 2 + 1, res[index][1], i + 1});
 			res[index][1] = (res[index][0] + res[index][1]) / 2;
 		}
 
