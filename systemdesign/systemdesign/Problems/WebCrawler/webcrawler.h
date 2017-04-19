e1f42cf6f86dae573624b0314066df88d82c52b9
wadewu <wuqingjun@gmail.com>
2017-04-19 12:31:35 -0700

    implementing multithreading part.

--- systemdesign/systemdesign/Problems/WebCrawler/webcrawler.h
+++ systemdesign/systemdesign/Problems/WebCrawler/webcrawler.h
@@ -4,19 +4,45 @@
 #include <condition_variable>
 #include <thread>
 #include <unordered_set>
+#include <queue>
 using namespace std;
 
 class Crawler
 {
 private:
-	mutex m_processed;
+	mutex m_mtxProcessed;
 	unordered_set<string> m_processedSet;
-	vector<string> m_urlQueue;
+	
+	mutex m_mtxUrlQueue;
+	condition_variable m_cvUrlQueue;
+	queue<string> m_vctUrlQueue;
 public:
-	Crawler() : m_urlQueue(8) {}
-	static void doWork(Crawler * c, string url)
+	Crawler() = default;
+
+	static void doWork(Crawler * c)
 	{
-		//lock_guard
+		unique_lock<mutex> lock(c->m_mtxUrlQueue);
+		c->m_cvUrlQueue.wait(lock, c->m_vctUrlQueue.size() > 0);
+		string url = c->m_vctUrlQueue.front();
+		c->m_cvUrlQueue.notify_one();
+
+		bool needProccess = false;
+
+		{
+			lock_guard<mutex> lockProccessed(c->m_mtxProcessed);
+			if (c->m_processedSet.find(url) == c->m_processedSet.end())
+			{
+				needProccess = true;
+				c->m_processedSet.insert(url);
+			}
+		}
+		
+		if (needProccess)
+		{
+
+		}
+
+
 	}
 
 	vector<string> crawl(string url)
