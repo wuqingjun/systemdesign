24ee311b61723239731db2dc8b029b74a42533ad
Wade Wu <wuqingjun@gmail.com>
2017-04-19 07:22:59 -0700

    working.

--- systemdesign/systemdesign/Problems/TypeAhead/typeahead.h
+++ systemdesign/systemdesign/Problems/TypeAhead/typeahead.h
@@ -9,19 +9,25 @@ using namespace std;
 
 class Typeahead {
 private:
-	unordered_map<string, vector<unordered_set<string>::iterator> > m_mp;
+	unordered_map<string, vector<int> > m_mp;
+	vector<string> m_strings;
 public:
 	// @param dict: A dictionary of words dict
 	Typeahead(unordered_set<string> &dict) {
 		// do initialize if necessary
 		for (auto itr = begin(dict); itr != end(dict); ++itr)
 		{
-			istringstream iss(*itr);
-			vector<string> tokens(istream_iterator<string>{iss}, istream_iterator<string>());
-			for (auto &t : tokens)
+			m_strings.push_back(*itr);
+			for (int i = 0; i < itr->size(); ++i)
 			{
-				m_mp[t].push_back(itr);
+				for (int j = i + 1; j <= itr->size(); ++j)
+				{
+					auto tmp = itr->substr(i, j - i);
+					if(m_mp[tmp].empty() || m_mp[tmp].back() != m_strings.size() - 1)
+						m_mp[tmp].push_back(m_strings.size() - 1);
+				}
 			}
+			m_mp[""].push_back(m_strings.size() - 1);
 		}
 	}
 
@@ -34,7 +40,7 @@ public:
 		{
 			for (auto i : m_mp[str])
 			{
-				res.push_back(*i);
+				res.push_back(m_strings[i]);
 			}
 		}
 		return res;
